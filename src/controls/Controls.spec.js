a7c516e5745330f6b5d1b6835d2e5dc091189157
Jamie Hall <hall.jamie@gmail.com>
2019-03-06 18:17:40 -0500

    Some of the tests are written. And some of them even work

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  it("cannot be closed or opened if it is locked", () => {
+    const { getByTestId } = render(<Controls closed={true} locked={true} />);
+    const close = getByTestId("lockUnlockButton");
+    const lock = getByTestId("openCloseButton");
+
+    expect(close.disabled).toBe(true);
+    expect(lock.disabled).toBe(false);
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,68 @@
 // Test away
+import React from 'react';
+import {render, fireEvent} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+import Dashboard from './dashboard'
+
+describe('<Dashboard />',()=>{
+  
+    describe ('Buttons should become disabled', () => {
+        const {getByTestId, getByText} = render (<Dashboard />);
+        const lock = getByTestId ('lockUnlock');
+        const open = getByTestId ('openClose');
+        const lockButton = getByTestId ('lockUnlockButton');
+        const closeButton = getByTestId ('openCloseButton');
+        it ('Lock button is disabled', () => {
+            expect (closeButton).toBeDisabled();
+        });
+        it ('Close button is disabled', () => {
+            fireEvent.click (closeButton);
+            fireEvent.click (lockButton);
+            expect (closeButton).toBeDisabled ();
+        });
+
+    });
+    describe ('Buttons should become active', () => {
+        const {getByTestId, getByText} = render (<Dashboard />);
+        const lock = getByTestId ('lockUnlock');
+        const open = getByTestId ('openClose');
+        const lockButton = getByTestId ('lockUnlockButton');
+        const closeButton = getByTestId ('openCloseButton');
+        it('Lock button reactivates', () => {
+            fireEvent.click(lockButton)
+            expect (lockButton).not.toBeDisabled ();
+        });
+        it('Close Button Reactivates', () => {            
+            fireEvent.click(lockButton)
+            fireEvent.click (closeButton);
+            fireEvent.click (lockButton);
+            expect (lockButton).not.toBeDisabled ();
+        });
+
+    });
+    // describe ('Indicators whould flip between green and red', () => {
+    //     // const lock = getByTestId ('lockUnlock');
+    //     // const open = getByTestId ('openClose');
+    //     const lockButton = getByTestId ('lockUnlockButton');
+    //     const closeButton = getByTestId ('openCloseButton');
+    //     it.skip ('Lock button is green on render', () => {
+    //         expect (lock).toHaveClassName('green-led')
+    //     });
+    //     it.skip ('gate button is green on render', () => {
+    //         expect (open).toHaveClassName ('green-led');
+    //     });
+    //     it.skip('closed gate is red',()=>{
+    //         fireEvent.click(lockButton)
+    //         expect(lock).toHaveClassName('red-led')
+    //     })
+    //     it.skip('locked gate is red', () => {
+    //         fireEvent.click (closeButton);
+    //         expect (open).toHaveClassName ('red-led');
+    //     });
+
+    // });
+    
+
+})
