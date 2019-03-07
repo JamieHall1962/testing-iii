581c865632d90acc2bd6870602bcd95aa5690a2c
Jamie Hall <hall.jamie@gmail.com>
2019-03-06 20:12:11 -0500

    17 tests written. All work. All written properly. All ugly tests removed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -9,38 +9,5 @@ import "react-testing-library/cleanup-after-each";
 import Dashboard from "./dashboard";
 
 describe("<Dashboard />", () => {
-  it("renders without crashing", () => {
-    // describe ('Buttons should become disabled', () => {
-    //     const {getByTestId, getByText} = render (<Dashboard />);
-    //     const lock = getByTestId ('lockUnlock');
-    //     const open = getByTestId ('openClose');
-    //     const lockButton = getByTestId ('lockUnlockButton');
-    //     const closeButton = getByTestId ('openCloseButton');
-    //     it ('Lock button is disabled', () => {
-    //         expect (closeButton.disabled).toBe(true);
-    //     });
-    //     it ('Close button is disabled', () => {
-    //         fireEvent.click (closeButton);
-    //         fireEvent.click (lockButton);
-    //         expect (closeButton.disabled).toBe(true);
-    //     });
-    // });
-    // describe ('Buttons should become active', () => {
-    //     const {getByTestId, getByText} = render (<Dashboard />);
-    //     const lock = getByTestId ('lockUnlock');
-    //     const open = getByTestId ('openClose');
-    //     const lockButton = getByTestId ('lockUnlockButton');
-    //     const closeButton = getByTestId ('openCloseButton');
-    //     it('Lock button reactivates', () => {
-    //         fireEvent.click(lockButton)
-    //         expect (lockButton.disabled).toBe(false);
-    //     });
-    //     it('Close Button Reactivates', () => {
-    //         fireEvent.click(lockButton)
-    //         fireEvent.click (closeButton);
-    //         fireEvent.click (lockButton);
-    //         expect (lockButton.disabled).toBe(false);
-    //     });
-    // });
-  });
+  it("renders without crashing", () => {});
 });
